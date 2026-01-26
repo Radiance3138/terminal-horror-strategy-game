c2de8cd5d9daea7ad3882d72483eaa16f3b716a2
Radiance3138 <umahnoor3138@gmail.com>
2026-01-26 20:01:56 +0500

    feat: add character class
    Implemented the base character model with movement.

--- entities.py
+++ entities.py
@@ -19,7 +19,7 @@ class Room:
 
     def add_item(self, item_object):
         """The room takes ownership of the item."""
-        
+
         self.items[item_object.name.lower()] = item_object
 
 class Item:
@@ -39,3 +39,36 @@ class Item:
             self.current_desc_index += 1
         
         return desc
+    
+class Character:
+    def __init__(self, name, location, inventory, panic_level, dialogue_options, is_player=False):
+        """
+        Docstring for __init__
+        
+        :string name: The name of the character.
+        :Bool is_player: Indicates if the character is the player or NPC.
+        :room_object location: The current location of the character (Room object).
+        :list inventory: List of items the character possesses (Item objects).
+        :int panic_level: The panic level of the character (0-100).
+        :dict dialogue_options: Dialogue options available for interaction.
+        """
+
+        self.name = name
+        self.is_player = is_player
+        self.location = location 
+        self.inventory = inventory
+        self.panic_level = panic_level
+        self.dialogue_options = dialogue_options 
+
+    def move_to(self, direction):
+        """Moves the character to a neighboring room in the specified direction.
+        
+        :room_object direction: The direction to move (e.g., 'north', 'south', etc.).
+        """
+
+        if direction.lower() in self.location.neighbors:
+            self.location = self.location.neighbors[direction.lower()]
+            return True
+        
+        return False
+        
